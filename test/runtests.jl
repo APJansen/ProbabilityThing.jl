541459071c79de02cf2c3525787d715355e0f79b
Aron <aronpjansen@gmail.com>
2023-06-12 15:45:14 +0200

    Move code from gist to ProbabilityThing.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,11 @@ using Test
 
 @testset "ProbabilityThing.jl" begin
     # Write your tests here.
+    for distribution in [Normal(), Uniform()]
+
+        @testset "Testing distribution $distribution" begin
+            @test pdf(distribution, 0.0) >= 0
+            @test cdf(distribution, Inf) ≈ 1.0
+        end
+    end
 end
